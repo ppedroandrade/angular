d014ab90466f4450fd73beb8074f1bd65f70b433
ppedroandrade <pedrosalvaro09@gmail.com>
2023-10-05 09:48:33 -0300

    Finalizando

--- src/app/pessoas/pessoaslist/pessoaslist.component.ts
+++ src/app/pessoas/pessoaslist/pessoaslist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { Pessoa } from '../pessoa';
 
 @Component({
@@ -8,13 +11,24 @@ import { Pessoa } from '../pessoa';
 })
 export class PessoaslistComponent {
   lista: Pessoa[] = [];
+  filter = new FormControl('');
+
+  filteredLista$: Observable<Pessoa[]>;
 
   constructor() {
     this.lista.push(new Pessoa('Pedro', 22));
-    this.lista.push(new Pessoa('Pedro', 22));
-    this.lista.push(new Pessoa('Pedro', 22));
-    this.lista.push(new Pessoa('Pedro', 22));
-    this.lista.push(new Pessoa('Pedro', 22));
-    this.lista.push(new Pessoa('Pedro', 22));
+    this.lista.push(new Pessoa('Maria', 25));
+    this.lista.push(new Pessoa('João', 30));
+
+    this.filteredLista$ = this.filter.valueChanges.pipe(
+      startWith(''),
+      map((text) => this.filterLista(text || '')) // Provide an empty string as a default value
+    );
+  }
+
+  filterLista(text: string): Pessoa[] {
+    return this.lista.filter((pessoa) => {
+      return pessoa.nome.toLowerCase().includes(text.toLowerCase());
+    });
   }
 }
